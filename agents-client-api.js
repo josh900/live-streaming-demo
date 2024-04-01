b1b4551bf3ba96066e810e74d41c5d247f8007e8
josh900 <jm5522@sbcglobal.net>
2024-04-01 12:11:26 -0700

    Revert "update"
    This reverts commit f4d97d6599a8478ea05cd7b8d2accbc69d232909.

--- agents-client-api.js
+++ agents-client-api.js
@@ -1,15 +1,10 @@
 'use strict';
 import DID_API from './api.js';
-const Groq = require('groq-sdk');
-
 
 const GROQ_API_KEY = DID_API.groqKey;
 const DEEPGRAM_API_KEY = DID_API.deepgramKey;
 
 if (DID_API.key == '🤫') alert('Please put your api key inside ./api.json and restart..');
-
-
-
 
 const RTCPeerConnection = (
   window.RTCPeerConnection ||
