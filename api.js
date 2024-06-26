f638d43ac79e650a1d2644da88ab95682b63dae3
josh900 <jm5522@sbcglobal.net>
2024-06-25 19:04:14 -0700

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,9 @@
 export default {
-    key: "YWRtaW4xQHNrb29wLmRpZ2l0YWw:quCILV7kl0vt4FJEvJLvf",
+    key: "YWRtaW4xQHNrb29wLmRpZ2l0YWw:1FItMzMiqjms0QwfA9g8p",
     url: "https://api.d-id.com",
     service: "talks",
     groqKey: "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",
     deepgramKey: "ab184815a3899aea7e3add69b9d5b7bc6894dc74"
 };
+
+
