95bd4df4f8b112ee427d00bd668d43a2b8c00e8b
josh900 <jm5522@sbcglobal.net>
2024-06-26 14:56:00 -0700

    Revert "Update api.js"
    This reverts commit abec80e76bfffcd38475c329a3060ee32cfeb104.

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 export default {
-    key: "bXVycmExNzhAZ21haWwuY29t:MgOl9SJMmjyilIgQn7Vtv",
+    key: "YWRtaW4xQHNrb29wLmRpZ2l0YWw:1FItMzMiqjms0QwfA9g8p",
     url: "https://api.d-id.com",
     service: "talks",
     groqKey: "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",
