0bd3d4d5ae95867a16c8ab240577ba9dde61ebc0
josh900 <jm5522@sbcglobal.net>
2024-06-28 10:19:44 -0700

    t

--- agents-client-api.js
+++ agents-client-api.js
@@ -1,10 +1,9 @@
 'use strict';
 import DID_API from './api.js';
 
-const GROQ_API_KEY = DID_API.groqKey;
 const DEEPGRAM_API_KEY = DID_API.deepgramKey;
 
-if (DID_API.key == '🤫') alert('Please put your api key inside ./api.json and restart..');
+if (DID_API.key == '🤫') alert('Please put your api key inside ./api.js and restart..');
 
 const RTCPeerConnection = (
   window.RTCPeerConnection ||
