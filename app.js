3a064fc6a4c1c092c6f4ebc4605baca9567b8087
josh900 <jm5522@sbcglobal.net>
2024-06-29 07:25:30 -0700

    revert

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
-import dotenv from 'dotenv';
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
 
-dotenv.config();
-
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const app = express();
 app.use(cors());
 
-app.use('/', express.static('.', {
-  setHeaders: (res, path) => {
-    if (path.endsWith('.js')) {
-      res.setHeader('Content-Type', 'application/javascript');
+app.use('/', express.static(__dirname, {
+    setHeaders: (res, path) => {
+      if (path.endsWith('.json')) {
+        res.type('application/javascript');
+      }
     }
-  }
-}));
+  }));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: '.' });
+app.use('/', express.static(__dirname));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+});
+app.get('/agents', function(req, res) {
+    res.sendFile(__dirname + '/index-agents.html')
 });
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log(`Server started on port ${port}\nhttp://localhost:${port}`));
+server.listen(port, () => console.log(`Server started on port localhost:${port}`));
+
