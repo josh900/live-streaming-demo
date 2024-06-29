335f7f9f8bc480d6c0940447d651e132fe8abbaf
josh900 <jm5522@sbcglobal.net>
2024-06-28 23:58:13 -0700

    Revert "Update config.js"
    This reverts commit 8e58373abcf751c6de18b374bc3f3fbaf9022d27.

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ const globalConfig = isBrowser && window.appConfig ? window.appConfig : {};
 
 export default {
   DID_API: {
-    key: globalConfig.DID_API_KEY || 'YWRtaW4xQHNrb29wLmRpZ2l0YWw:1FItMzMiqjms0QwfA9g8p',
+    key: globalConfig.DID_API_KEY || 'your_default_did_api_key_here',
     url: 'https://api.d-id.com',
     service: 'talks'
   },
-  GROQ_API_KEY: globalConfig.GROQ_API_KEY || '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',
+  GROQ_API_KEY: globalConfig.GROQ_API_KEY || 'your_default_groq_api_key_here',
   GROQ_API_URL: 'https://api.groq.com/openai/v1/chat/completions',
-  DEEPGRAM_API_KEY: globalConfig.DEEPGRAM_API_KEY || 'ab184815a3899aea7e3add69b9d5b7bc6894dc74',
+  DEEPGRAM_API_KEY: globalConfig.DEEPGRAM_API_KEY || 'your_default_deepgram_api_key_here',
   idleVideoUrl: 'emma_idle.mp4',
   avatarImageUrl: 'https://create-images-results.d-id.com/DefaultPresenters/Emma_f/v1_image.jpeg',
   voiceId: 'en-US-JennyMultilingualV2Neural',
