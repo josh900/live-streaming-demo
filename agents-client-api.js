f81a2929650955e5458a6fafec8fb12d4c62c9de
josh900 <jm5522@sbcglobal.net>
2024-06-28 19:58:34 -0700

    Update agents-client-api.js

--- agents-client-api.js
+++ agents-client-api.js
@@ -734,6 +734,7 @@ connectButton.onclick = async () => {
       },
     }),
   });
+  
 
   const { id: newStreamId, offer, ice_servers: iceServers, session_id: newSessionId } = await sessionResponse.json();
   streamId = newStreamId;
