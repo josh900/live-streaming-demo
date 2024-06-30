f4213bd2575aa0a0afb35419c42db6d72dd44379
josh900 <jm5522@sbcglobal.net>
2024-06-30 05:47:54 -0700

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,21 +1,23 @@
-export default class Logger {
-    constructor(level = 'INFO') {
-      this.level = level;
-    }
-  
-    log(...args) {
-      if (this.level === 'INFO' || this.level === 'DEBUG') {
-        console.log(...args);
-      }
-    }
-  
-    error(...args) {
-      console.error(...args);
-    }
-  
-    debug(...args) {
-      if (this.level === 'DEBUG') {
-        console.debug(...args);
-      }
+class Logger {
+  constructor(level = 'INFO') {
+    this.level = level;
+  }
+
+  log(...args) {
+    if (this.level === 'INFO' || this.level === 'DEBUG') {
+      console.log(...args);
     }
   }
+
+  error(...args) {
+    console.error(...args);
+  }
+
+  debug(...args) {
+    if (this.level === 'DEBUG') {
+      console.debug(...args);
+    }
+  }
+}
+
+module.exports = Logger;
