022e519607820189ac8fc9868ad77c5a62b1c09c
josh900 <jm5522@sbcglobal.net>
2024-07-02 08:43:13 -0700

    Update agents-client-api.js

--- agents-client-api.js
+++ agents-client-api.js
@@ -1092,7 +1092,7 @@ async function sendChatToGroq() {
           },
           ...chatHistory,
         ],
-        model: 'llama3-8b-8192',
+        model: 'mixtral-8x7b-32768',
       }),
     });
 
