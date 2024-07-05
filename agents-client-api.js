caa453f37da89f98dddd6671bbc8ec026c8306a1
josh900 <jm5522@sbcglobal.net>
2024-07-05 07:48:35 -0700

    Update agents-client-api.js

--- agents-client-api.js
+++ agents-client-api.js
@@ -1200,7 +1200,7 @@ async function fetchWithRetries(url, options, retries = 1) {
   }
 }
 
-javascriptCopyasync function initializeConnection() {
+async function initializeConnection() {
   if (isInitializing) {
     logger.warn('Connection initialization already in progress. Skipping initialize.');
     return;
