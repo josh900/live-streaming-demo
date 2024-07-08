b1b71ebdf6d7c3fb8cd9aaf751f33afc2bce0b7e
josh900 <jm5522@sbcglobal.net>
2024-07-07 21:00:39 -0700

    Revert "Update agents-client-api.js"
    This reverts commit 573cfdd13020b833429886e01dade57ae1b62167.

--- agents-client-api.js
+++ agents-client-api.js
@@ -1410,7 +1410,15 @@ function startKeepAlive() {
   }
   keepAliveInterval = setInterval(async () => {
     try {
-     await reinitializeConnection();
+      await fetchWithRetries(`${DID_API.url}/${DID_API.service}/streams/${streamId}/keepalive`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${DID_API.key}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ session_id: sessionId }),
+      });
+      logger.debug('Keep-alive sent successfully');
     } catch (error) {
       logger.warn('Error sending keep-alive:', error);
     }
@@ -1542,7 +1550,7 @@ async function startStreaming(assistantReply) {
       currentStreamTimeout = setTimeout(() => {
         isCurrentlyStreaming = false;
         smoothTransition(false, 250);
-      }, audioDuration + 100); // Increased buffer time slightly
+      }, audioDuration + 250); // Increased buffer time slightly
 
     } else {
       logger.warn('Unexpected response status:', playResponseData.status);
