4c86a82e7ae9928129601286cc9c155b01c3b7f3
josh900 <jm5522@sbcglobal.net>
2024-07-12 13:15:09 -0700

    Update agents-client-api.js

--- agents-client-api.js
+++ agents-client-api.js
@@ -1646,7 +1646,7 @@ async function startStreaming(assistantReply) {
     }
 
     // Split the reply into chunks of about 150 characters, breaking at spaces
-    const chunks = assistantReply.match(/[\s\S]{1,150}(?:\s|$)/g) || [];
+    const chunks = assistantReply.match(/[\s\S]{1,250}(?:\s|$)/g) || [];
 
     // Start the transition to streaming video immediately
     smoothTransition(true);
