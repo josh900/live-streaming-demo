b6c80e435b709b2af6acfa0436f579b2544882cb
josh900 <jm5522@sbcglobal.net>
2024-07-23 09:48:06 -0700

    Update agents-client-api.js

--- agents-client-api.js
+++ agents-client-api.js
@@ -1255,20 +1255,6 @@ async function attemptReconnect() {
   }
 }
 
-
-
-async function attemptReconnect() {
-  logger.debug('Attempting to reconnect...');
-  try {
-    await reinitializeConnection();
-    logger.debug('Reconnection successful');
-  } catch (error) {
-    logger.error('Reconnection attempt failed:', error);
-    scheduleReconnect();
-  }
-}
-
-
 function onConnectionStateChange() {
   const { peer: peerStatusLabel } = getStatusLabels();
   if (peerStatusLabel) {
