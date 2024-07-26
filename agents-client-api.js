411de86a70cc17989f37045e310583c9fa859dff
josh900 <jm5522@sbcglobal.net>
2024-07-26 10:51:02 -0700

    Update agents-client-api.js

--- agents-client-api.js
+++ agents-client-api.js
@@ -2146,6 +2146,7 @@ async function prepareStream() {
       body: JSON.stringify({
         script: {
           type: 'text',
+          ssml: true,
           input: '<break time="5000ms"/>',  // Silent audio
           provider: {
             type: 'microsoft',
