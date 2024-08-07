542c41edbff46b5486187db364be9d086521ac60
josh900 <jm5522@sbcglobal.net>
2024-08-06 20:29:23 -0700

    update

--- agents-client-api.js
+++ agents-client-api.js
@@ -1,8 +1,8 @@
-import { ConnectionState, initializePersistentStream, destroyPersistentStream, reinitializePersistentStream, createNewPersistentStream, backgroundReconnect, createPeerConnection, onIceGatheringStateChange, onIceCandidate, onIceConnectionStateChange, onConnectionStateChange, onSignalingStateChange, onTrack, startConnectionHealthCheck, attemptReconnect, stopAllStreams, closePC, fetchWithRetries, initializeConnection, reinitializeConnection, cleanupOldStream } from './connection-manager.js';
+import { ConnectionState, initializePersistentStream, destroyPersistentStream, reinitializePersistentStream, createNewPersistentStream, backgroundReconnect, createPeerConnection, onIceGatheringStateChange, onIceCandidate, onIceConnectionStateChange, onConnectionStateChange, onSignalingStateChange, onTrack, startConnectionHealthCheck, attemptReconnect, stopAllStreams, closePC, fetchWithRetries, initializeConnection, reinitializeConnection, cleanupOldStream, connectionState } from './connection-manager.js';
 import { initializeTransitionCanvas, smoothTransition, getVideoElements, onVideoStatusChange, setStreamVideoElement, playIdleVideo, prepareForStreaming } from './video-manager.js';
-
-import DID_API from './api.js';
 import logger from './logger.js';
+import DID_API from './api.js';
+
 const { createClient, LiveTranscriptionEvents } = deepgram;
 
 const deepgramClient = createClient(DID_API.deepgramKey);
