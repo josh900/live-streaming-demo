2472dab8998582feb6f631d7e53d32bc78b92665
josh900 <jm5522@sbcglobal.net>
2024-08-15 08:39:58 -0700

    Revert "Update avatar-manager.js"
    This reverts commit 8cd7711353ea3fa21bf0affbddfa70dc40e763f2.

--- avatar-manager.js
+++ avatar-manager.js
@@ -6,6 +6,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { Readable } from 'stream';
 import sharp from 'sharp';
+import fs from 'fs/promises';
 import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
