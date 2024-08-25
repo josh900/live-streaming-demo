8c38b0ac007d0530e8f3892af86dad6de35be1fa
josh900 <jm5522@sbcglobal.net>
2024-08-25 03:17:24 -0700

    Update agents-client-api.js

--- agents-client-api.js
+++ agents-client-api.js
@@ -1087,7 +1087,7 @@ function exitSimpleMode() {
 
   // Reset logo
   logoWrapper.style.backgroundImage = "url('Slogo.svg')";
-  logoWrapper.style.width = '50px'; // Reset to original width
+  logoWrapper.style.width = '15%'; // Reset to original width
 
   // Remove simple push talk class from body
   document.body.classList.remove('simple-push-talk');
