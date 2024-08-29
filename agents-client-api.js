0f04456a8d1321dc0f8c5ae98a229c5e8d78e797
josh900 <jm5522@sbcglobal.net>
2024-08-28 20:54:34 -0700

    Update agents-client-api.js

--- agents-client-api.js
+++ agents-client-api.js
@@ -844,7 +844,7 @@ function endPushToTalk(event) {
 
 
 async function initialize() {
-  setLogLevel('INFO');
+  setLogLevel('DEBUG');
   connectionState = ConnectionState.DISCONNECTED;
 
   const { avatarId, contextId, interfaceMode, header } = getUrlParameters();
